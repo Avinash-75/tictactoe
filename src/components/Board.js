e823871069b99b3853ee5c264d88f7c91c8ca76d
avikkr75 <avikkr75@gmail.com>
2021-02-16 06:53:29 +0530

    added board state and react events also

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,23 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Square from './Square';
 
 const Board = () => {
+  const [board, setBoard] = useState(Array(9).fill(null)); // board is state and
+  // setBoard is update function to update the values from null to X or O
+
+  // console.log(setBoard);
+  // console.log(board);
+
+  // we need another state to keep track of next player just like above setBoard
+  // isXNext = boolean value, represents current player
+  // setisXNext = update function from O to X & X to O
+
+  const [isXNext, setisXNext] = useState(false);
+
+  const handleSquareClick = position => {
+    if (board[position]) {
+      return;
+    }
+
+    setBoard(prev => {
+      // prev -> previous state of the Board
+
+      return prev.map((square, pos) => {
+        if (pos === position) {
+          return isXNext ? 'X' : 'O';
+        }
+
+        return square;
+      });
+    });
+
+    setisXNext(prev => !prev);
+  };
+
+  const renderPosition = position => {
+    // custom Function
+    return (
+      <Square
+        value={board[position]}
+        onClick={() => handleSquareClick(position)}
+      />
+    );
+  };
+
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={0} />
-        <Square value={1} />
-        <Square value={2} />
+        {renderPosition(0)}
+        {renderPosition(1)}
+        {renderPosition(2)}
+        {/* <Square  // instead of these 4 line code use-{renderposition(var)}
+          value={board[0]}   // but we have to write this
+          onClick={() => {   // in every Square component
+            handleSquareClick();  // so it will be not good
+          }}
+        /> */}
       </div>
       <div className="board-row">
-        <Square value={3} />
-        <Square value={4} />
-        <Square value={5} />
+        {renderPosition(3)}
+        {renderPosition(4)}
+        {renderPosition(5)}
       </div>
       <div className="board-row">
-        <Square value={6} />
-        <Square value={7} />
-        <Square value={8} />
+        {renderPosition(6)}
+        {renderPosition(7)}
+        {renderPosition(8)}
       </div>
     </div>
   );
